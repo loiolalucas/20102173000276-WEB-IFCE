a219e215ca12210ab1f34f1ed468ee89f37e066e
Your Name <lucasloiolallo@gmail.com>
2018-04-08 20:24:53 -0300

    Primeiro Exercicio de JavaScript

---
+++ exercicio02.04.18-primeiro JavaScript/myScript.js
@@ -0,0 +1,14 @@
+/*imprimindo mensagem*/
+alert ('Ola Mundo');
+/*somando dois inteiros*/
+var a=1;
+var b=2;
+// alert (a+b);
+
+
+/*somando dois inteiros com função*/
+function somarValores(a,b) {
+	// body...
+	alert (a+b);
+}
+somarValores(a,b);
